c041fa0d9544c6776fafb9c8fa52a3b3fba7238f
Eliza-2202 <elizabeth@datablitz.com.pe>
2024-10-21 19:02:10 -0500

    suerte342121

--- jenkindocker_File_parte2/main.py
+++ jenkindocker_File_parte2/main.py
@@ -8,4 +8,6 @@ async def read_root():
     return {"Hello": "World"}
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    import uvicorn
+    uvicorn.run("main:app", host="0.0.0.0", port=8000)
+
